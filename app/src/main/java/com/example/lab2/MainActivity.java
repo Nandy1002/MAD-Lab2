d0ca8253b85128155d03014d4346c908a8be940b
Nandy_X_Coder <nabendudas2001@gmail.com>
2024-02-17 08:28:43 +0530

    main commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.lab2;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -11,27 +14,65 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int value = 0;
+    public Button button;
+    ProgressBar Progress;
+    CountDownTimer t;
+    float total;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.loadButton);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
 
+
+// Program starts
+
+
+        Button button=findViewById(R.id.loadButton);
+        ProgressBar Progress=findViewById(R.id.progressBar);
+
+        Progress.setVisibility(View.INVISIBLE);
+        Progress.setMax(100);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                value += 10;
-                progressBar.setProgress(value);
-                if(value ==  100){
-                    Toast.makeText(MainActivity.this, "Progress Bar Filled", Toast.LENGTH_SHORT).show();
-                    value = 0;
-                    AlertDialog.Builder alertbox = new AlertDialog.Builder(MainActivity.this);
-                    alertbox.setTitle("Completed");
-                    alertbox.setMessage("Loading has been finished.");
-                    alertbox.create().show();
-                }
+            public void onClick(View view) {
+                Progress.setVisibility(View.VISIBLE);
+                long min= 10*1000;//second * milisecond;
+                new CountDownTimer(min,1000)
+                {
+                    @Override
+                    public void onTick(long l) {
+                        long a = min-l;
+                        total= ((float)a/(float)min)*100;
+                        Log.e("Total", String.valueOf( (a/min)));
+                        Progress.setProgress((int) total);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        AlertDialog.Builder d = new AlertDialog.Builder(MainActivity.this);
+                        d.setTitle(" Completed ").setMessage(" Loding as Been Completed").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this,"Done",Toast.LENGTH_LONG).show();
+                            }
+                        }).setNegativeButton("Reject", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Rejected", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        AlertDialog dialog = d.create();
+                        dialog.show();
+                    }
+                }.start();
+//
+
             }
         });
     }
